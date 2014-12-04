ee3ad95065f396750a23f40b8be7818fd5cb3f37
Aniket Lawande <anlawande@gmail.com>
2014-12-04 11:41:52 -0500

    Adding videos to the animate view

--- vendor/assets/javascripts/animatedimages/js/jquery.gridrotator.js
+++ vendor/assets/javascripts/animatedimages/js/jquery.gridrotator.js
@@ -643,6 +643,10 @@
 					}
 					
 					$el.next().remove();
+                    var videos = $el.find("video");
+                    if(videos.length !== 0) {
+                        videos[0].play();
+                    }
 					$el.parent().data( 'active', false );
 
 				}, t );
