7e158521b35cf4af73d7b783d0e49e3b3ff4e090
Jason Casden <jmcasden@ncsu.edu>
2014-12-09 16:25:14 -0500

    Restore image error handling

--- app/assets/javascripts/lentil/imageerrors.js
+++ app/assets/javascripts/lentil/imageerrors.js
@@ -1,12 +1,8 @@
 function addimageerrors() {
-    $("body").on(
-        "error",
-        ".instagram-img, .battle-img, .fancybox-img",
-        function () {
-            $(this).parents("div.image-tile, li.image-animate-tile").remove();
-            if ($("body.images_animate") || $("body.images_staff_picks_animate")) {
-                addanimatedimages();
-            }
+    $(".instagram-img, .battle-img, .fancybox-img").off("error").on("error", function () {
+        $(this).parents("div.image-tile, li.image-animate-tile").remove();
+        if ($("body.images_animate") || $("body.images_staff_picks_animate")) {
+            addanimatedimages();
         }
-    );
+    });
 }
