57ba366f514e440976080eaab011fac2aa340a36
Aniket Lawande <anlawande@gmail.com>
2014-12-09 10:00:49 -0500

    Corrected indententation

--- test/integration/lentil/images_show_test.rb
+++ test/integration/lentil/images_show_test.rb
@@ -4,12 +4,12 @@ class ImagesShowIndexTest < ActionDispatch::IntegrationTest
 
   # This is important otherwise there is some strange reloading that goes on
   test "should have a class on the body" do
-  	visit(lentil.image_path lentil_images(:one))
+    visit(lentil.image_path lentil_images(:one))
     assert page.has_selector?('body.lentil-images_show')
   end
 	
   test "video should have a video tag" do
-  	visit(lentil.image_path lentil_images(:video))
+    visit(lentil.image_path lentil_images(:video))
     assert page.has_selector?('video')
   end
 
