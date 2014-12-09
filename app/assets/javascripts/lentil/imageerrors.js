3955bb7b50736a4d8868d3457fcc824a5dcd7d0f
Jason Casden <jmcasden@ncsu.edu>
2014-12-09 16:08:01 -0500

    Clean up fancybox and image error issues

--- app/assets/javascripts/lentil/imageerrors.js
+++ app/assets/javascripts/lentil/imageerrors.js
@@ -1,8 +1,12 @@
 function addimageerrors() {
-    $("img.instagram-img, img.battle-img, img.fancybox-img").on("error", function () {
-        $(this).parents("div.image-tile, li.image-animate-tile").remove();
-        if ($("body.images_animate") || $("body.images_staff_picks_animate")) {
-            addanimatedimages();
+    $("body").on(
+        "error",
+        ".instagram-img, .battle-img, .fancybox-img",
+        function () {
+            $(this).parents("div.image-tile, li.image-animate-tile").remove();
+            if ($("body.images_animate") || $("body.images_staff_picks_animate")) {
+                addanimatedimages();
+            }
         }
-    });
+    );
 }
