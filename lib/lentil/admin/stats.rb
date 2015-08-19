13a2ab8c4ff6ee567ce1c34691ba5f2f638d402c
Bret Davidson <bret_davidson@ncsu.edu>
2015-08-19 15:08:29 -0400

    add metrics

--- lib/lentil/admin/stats.rb
+++ lib/lentil/admin/stats.rb
@@ -7,20 +7,28 @@ ActiveAdmin.register_page "Stats" do
     def index
       stats = []
 
-      stats << {name: 'Number of Images', value: Lentil::Image.all.count}
-      stats << {name: 'Number of Battles', value: Lentil::Battle.all.count}
-      stats << {name: 'Number of Like Votes', value: Lentil::LikeVote.all.count}
-      stats << {name: 'Number of Users', value: Lentil::User.all.count}
-      stats << {name: 'Number of Donor Agreements', value: calc_donor_agreements}
-      stats << {name: 'Number of Tags', value: Lentil::Tag.all.count}
-      @stats = stats
-    end
-
-    def calc_donor_agreements
       images = Lentil::Image.all
+      battles = Lentil::Battle.all.count
+      like_votes = Lentil::LikeVote.all.count
+      users = Lentil::User.all.count
+      tags = Lentil::Tag.all.count
+      taggings = Lentil::Tagging.all.count
+
       submitted_agreements = images.map(&:donor_agreement_submitted_date).compact.count
       rejected_agreements = images.map(&:donor_agreement_rejected).compact.count
-      submitted_agreements - rejected_agreements
+      accepted_aggrements = submitted_agreements - rejected_agreements
+
+      stats << {name: 'Number of Images', value: images.count}
+      stats << {name: 'Number of Battles', value: battles}
+      stats << {name: 'Number of Like Votes', value: like_votes}
+      stats << {name: 'Number of Users', value: users}
+      stats << {name: 'Number of Submitted Donor Agreements', value: submitted_agreements}
+      stats << {name: 'Number of Accepted Donor Agreements (implicit)', value: accepted_aggrements}
+      stats << {name: 'Number of Rejected Donor Agreements (explicit)', value: rejected_agreements}
+      stats << {name: 'Number of Unique Tags', value: tags}
+      stats << {name: 'Number of Taggings', value: taggings}
+
+      @stats = stats
     end
   end
 end
