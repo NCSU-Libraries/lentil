1ef4cefd504b638fa3e8d992ecbc467648536903
Bret Davidson <bret_davidson@ncsu.edu>
2015-08-21 15:32:45 -0400

    improve image error handling and improve moderation filtering

--- lib/lentil/admin/images.rb
+++ lib/lentil/admin/images.rb
@@ -2,10 +2,41 @@ if defined?(ActiveAdmin)
   ActiveAdmin.register Lentil::Image do
     actions :index, :show
 
-    config.batch_actions = false
+    config.batch_actions = true
     config.per_page = 10
 
-    filter :state, :as => :select, :collection => proc { Lentil::Image::States }
+    batch_action :destroy, false
+
+    batch_action :approve_all do |ids|
+      images = Lentil::Image.where(id: ids)
+      total_images = images.size
+      image_counter = 0
+      errors = []
+
+      images.each do |image|
+        second_moderation = true
+        unless image.moderator_id.nil?
+          second_moderation = false
+        end
+
+        begin
+          image.update_attributes!(:state => 1, :moderator => current_admin_user, :moderated_at => DateTime.now,
+            :second_moderation => second_moderation)
+        rescue Exception => e
+          errors << "image_id #{image.id}: #{e.message}"
+          next
+        end
+
+        image_counter += 1
+      end
+
+      if errors.length > 0
+        redirect_to collection_path, notice: "#{image_counter} #{'Image'.pluralize(image_counter)} approved (out of #{total_images})", alert: errors.join('<br>').html_safe
+      else
+        redirect_to collection_path, notice: "#{image_counter} #{'Image'.pluralize(image_counter)} approved (out of #{total_images})"
+      end
+    end
+
     filter :suppressed
     filter :media_type, :as => :select
     filter :user_user_name, :as => :string, :label => "Username"
@@ -22,19 +53,40 @@ if defined?(ActiveAdmin)
     filter :url, :label => "Service URL"
     filter :id
 
+    scope :all
+    scope :new, :default => true do |images|
+      images.where(:state => Lentil::Image::States[:pending])
+    end
+    scope :approved do |images|
+      images.where(:state => Lentil::Image::States[:approved])
+    end
+    scope :rejected do |images|
+      images.where(:state => Lentil::Image::States[:rejected])
+    end
+    scope :skipped do |images|
+      images.where(:state => Lentil::Image::States[:pending]).where("moderator_id IS NOT NULL")
+    end
+    # scope :flagged do |images|
+    #   # images.where(:state => Lentil::Image::States[:pending]).where("moderator_id IS NOT NULL")
+    #   images.includes(:user, :tags, :taggings, :flags).joins(:flags).where(:second_moderation => false).uniq.all
+    # end
+
     action_item :only => :show do
       link_to('Update Image', update_image_admin_lentil_image_path(lentil_image))
     end
     action_item { link_to "Moderate New", moderate_admin_lentil_images_path }
-    action_item { link_to "Moderate Approved", moderate_approved_admin_lentil_images_path }
-    action_item { link_to "Moderate Rejected", moderate_rejected_admin_lentil_images_path }
     action_item { link_to "Moderate Skipped", moderate_skipped_admin_lentil_images_path }
     action_item { link_to "Moderate Flagged", moderate_flagged_admin_lentil_images_path }
+    action_item { link_to "Moderate Approved", moderate_approved_admin_lentil_images_path }
+    action_item { link_to "Moderate Rejected", moderate_rejected_admin_lentil_images_path }
     action_item { link_to "Flagging History", flagging_history_admin_lentil_images_path }
     action_item { link_to "Add Instagram Image", manual_input_admin_lentil_images_path }
 
     index do
       harvestable_tag_ids = Lentil::Tag.harvestable.map(&:id)
+      if current_scope.name == 'New'
+        selectable_column
+      end
       column "Image" do |image|
         unless image.media_type == "video"
             link_to(image_tag(image.image_url, :class => "moderation_thumbnail"), admin_lentil_image_path(image))
@@ -130,6 +182,10 @@ if defined?(ActiveAdmin)
 
       # TODO: This method may need optimization
       def update_images(images, images_params, second_moderation)
+        total_images = images.size
+        image_counter = 0
+        errors = []
+
         images.each do |image|
           image_params = images_params[image.id.to_s]
 
@@ -157,15 +213,33 @@ if defined?(ActiveAdmin)
             taggings = new_taggings + taggings_to_keep
 
             # Save Updated Image with new tags
-            image.update_attributes!(:state => image_params['state'], :taggings => taggings, :staff_like => image_params['staff_like'],
+            begin
+              image.update_attributes!(:state => image_params['state'], :taggings => taggings, :staff_like => image_params['staff_like'],
               :moderator => current_admin_user, :moderated_at => DateTime.now, :second_moderation => second_moderation,
               :do_not_request_donation => image_params['do_not_request_donation'], :suppressed => image_params['suppressed'])
+            rescue Exception => e
+              errors << "image_id #{image.id}: #{e.message}"
+              next
+            end
           else
             # Save Updated Image with same tags
-            image.update_attributes!(:state => image_params['state'], :staff_like => image_params['staff_like'],
+            begin
+              image.update_attributes!(:state => image_params['state'], :staff_like => image_params['staff_like'],
               :moderator => current_admin_user, :moderated_at => DateTime.now, :second_moderation => second_moderation,
               :do_not_request_donation => image_params['do_not_request_donation'], :suppressed => image_params['suppressed'])
+            rescue Exception => e
+              errors << "image_id #{image.id}: #{e.message}"
+              next
+            end
           end
+
+          image_counter += 1
+        end
+
+        if errors.length > 0
+          redirect_to :back, notice: "#{image_counter} #{'Image'.pluralize(image_counter)} moderated (out of #{total_images})", alert: errors.join('<br>').html_safe
+        else
+          redirect_to :back, notice: "#{image_counter} #{'Image'.pluralize(image_counter)} moderated (out of #{total_images})"
         end
       end
     end
@@ -218,18 +292,7 @@ if defined?(ActiveAdmin)
       images = Lentil::Image.find(params[:image].keys, :include => [:user, :taggings, :tags])
       images_params = params[:image]
       second_moderation = params[:moderation]['second_moderation']
-
-      begin
-        update_images(images, images_params, second_moderation)
-      rescue => e
-        message = "Error moderating images: " + e.to_s
-        redirect_to :back, :flash => {:error => message}
-      else
-        number_of_images = params['image'].size
-        plural = 'image'.pluralize(number_of_images)
-        message = number_of_images.to_s + " " + plural + " moderated."
-        redirect_to :back, notice: message
-      end
+      update_images(images, images_params, second_moderation)
     end
 
     collection_action :flagging_history do
