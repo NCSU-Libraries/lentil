5c36ea8d5d33f2b5a9bd5ef49d7b182cd9ece626
Jason Casden <jmcasden@ncsu.edu>
2015-08-24 21:56:04 -0400

    Test asset precompilation, expect initial failure

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -11,6 +11,12 @@ Dummy::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
 
