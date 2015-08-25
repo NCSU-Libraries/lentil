0bafff47367768f6f755402ffa4320980248cfd0
Jason Casden <jmcasden@ncsu.edu>
2015-08-24 22:15:30 -0400

    Compile assets in test env

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -12,10 +12,10 @@ Dummy::Application.configure do
   config.static_cache_control = "public, max-age=3600"
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
