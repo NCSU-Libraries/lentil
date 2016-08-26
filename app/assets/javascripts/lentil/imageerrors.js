20cdc5c2f3f2f481b0042229ab5339e17b6acde9
Jason Casden <jmcasden@ncsu.edu>
2016-08-26 10:25:36 -0400

    Set rel noopener roreferrer on links

--- app/assets/javascripts/lentil/imageerrors.js
+++ app/assets/javascripts/lentil/imageerrors.js
@@ -1,4 +1,6 @@
 function addimageerrors() {
+    $("a[target=_blank]").attr("rel", "noopener noreferrer");
+
     $(".instagram-img, .battle-img, .fancybox-img, .fancybox-video").off("error").on("error", function () {
         $(this).parents("div.image-tile, li.image-animate-tile").remove();
     });
